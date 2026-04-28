a3281e708bcd3ae7362c2630f25ec345b0b5594a
Khanh23-code <nguyendangkhanh762006@gmail.com>
2026-04-28 20:08:56 +0700

    feat(signup): refin signup screen

--- customer-app/src/screens/auth/SignUpScreen.tsx
+++ customer-app/src/screens/auth/SignUpScreen.tsx
@@ -7,9 +7,8 @@ import {
   TouchableOpacity, 
   ScrollView,
   Platform,
-  Image
 } from 'react-native';
-import { COLORS, SIZES, FONTS, SHADOWS } from '../../constants/theme';
+import { COLORS, SIZES, FONTS } from '../../constants/theme';
 
 import CustomInput from '../../components/common/CustomInput';
 import PrimaryButton from '../../components/common/PrimaryButton';
@@ -68,33 +67,6 @@ const SignupScreen = () => {
             style={{ marginTop: 15 }}
           />
 
-          {/* Divider */}
-          <View style={styles.dividerContainer}>
-            <View style={styles.dividerLine} />
-            <Text style={styles.dividerText}>or continue with</Text>
-            <View style={styles.dividerLine} />
-          </View>
-
-          {/* Social Login 
-              (Trong ảnh của bạn bị trống chỗ này, có thể do lỗi xuất ảnh Figma. 
-              Tôi cứ thêm vào cho đồng bộ với trang Login nhé, nếu bạn không thích thì cứ xóa khối này đi) 
-          */}
-          <View style={styles.socialContainer}>
-            <TouchableOpacity style={styles.socialSquareButton}>
-              <Image 
-                source={require('../../assets/images/facebook_icon.png')} 
-                style={styles.socialIcon} 
-              />
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.socialSquareButton}>
-              <Image 
-                source={require('../../assets/images/google_icon.png')} 
-                style={styles.socialIcon} 
-              />
-            </TouchableOpacity>
-          </View>
-
           {/* Footer chuyển về Login */}
           <View style={styles.footerContainer}>
             <Text style={styles.footerText}>You have already an account? </Text>
@@ -137,44 +109,6 @@ const styles = StyleSheet.create({
     color: COLORS.textTitle,
     marginBottom: SIZES.padding * 1.5,
     lineHeight: 40,
-  },
-  dividerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '100%',
-    marginVertical: 30,
-  },
-  dividerLine: {
-    flex: 1,
-    height: 1,
-    backgroundColor: '#EEEEEE',
-  },
-  dividerText: {
-    fontFamily: FONTS.regular,
-    fontSize: SIZES.body2,
-    color: COLORS.textBody,
-    marginHorizontal: 15,
-  },
-  socialContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 20,
-  },
-  socialSquareButton: {
-    width: 80,
-    height: 60,
-    backgroundColor: COLORS.background,
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#EEEEEE',
-    ...SHADOWS.light,
-  },
-  socialIcon: {
-    width: 24,
-    height: 24,
-    resizeMode: 'contain',
   },
   footerContainer: {
     flexDirection: 'row',

2fcd178d7a4c132e1e3036fed30aeac20cad1603
Khanh23-code <nguyendangkhanh762006@gmail.com>
2026-05-30 12:29:12 +0700

    feat(be-api): add new api drivers/:id to get driver's profile

--- backend/src/drivers/drivers.module.ts
+++ backend/src/drivers/drivers.module.ts
@@ -9,6 +9,7 @@ import { DriverProfile } from './entities/driver_profile.entity'
 import { DriverDocument } from './entities/driver_document.entity'
 import { Vehicle } from './entities/vehicle.entity'
 import { Trip } from '../rides/entities/trip.entity'
+import { UsersModule } from '../users/users.module'
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { Trip } from '../rides/entities/trip.entity'
     LocationModule,
     RidesModule,
     JwtModule.register({}),
+    UsersModule,
   ],
   controllers: [DriversController],
   providers: [DriversService],

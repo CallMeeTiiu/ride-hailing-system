70df8437b7d77ec6695d33e8bc17e7c087b835a9
Nguyễn Xuân Nhật Tân <tiiuworkmail@gmail.com>
2026-05-31 01:45:14 +0700

    fix: QueryFailedError

--- backend/src/rides/entities/rating.entity.ts
+++ backend/src/rides/entities/rating.entity.ts
@@ -35,7 +35,7 @@ export class Rating {
   @Column({ name: 'driver_id', nullable: true })
   driver_id: string
 
-  @Column({ type: 'int' })
+  @Column({ type: 'int', default: 0 })
   rating: number
 
   @Column({ type: 'text', nullable: true })
